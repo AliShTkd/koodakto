be6094ac99919abc117133f1229d427d0d6f4e4d
raambod <rambodheydari1@gmail.com>
2025-08-10 12:34:28 +0330

    search product2

--- assets/searchproduct.js
+++ assets/searchproduct.js
@@ -1,13 +1,16 @@
-const searchInput = document.querySelector("input-1");
-const products = document.querySelectorAll("product-1-page");
-searchInput.addEventListener("input" , function() {
-    const searchValue = this.value.trim().toLowerCase();
-    products.forEach(product => {
-        const productName = product.querySelector("product-text").textContent.toLowerCase();
-        if(productName.includes(searchValue)){
-            product.style.display = "block";
-        } else {
-             product.style.display = "none";
-        }
-    });
+const searchInput = document.querySelector(".input-1"); // اینپوت سرچ
+const products = document.querySelectorAll(".product-1-page"); // همه محصولات
+
+searchInput.addEventListener("input", function() {
+  const searchValue = this.value.trim().toLowerCase(); // متن تایپ شده
+
+  products.forEach(product => {
+    const productName = product.querySelector(".product-text").textContent.toLowerCase(); // نام محصول
+
+    if (productName.includes(searchValue)) {
+      product.style.display = "block"; // نمایش
+    } else {
+      product.style.display = "none"; // مخفی
+    }
+  });
 });
