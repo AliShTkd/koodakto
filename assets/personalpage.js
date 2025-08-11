7c72b4b0a6cbcc56784c3a9740e5d24df98376f4
erfan-abm <e.abomardani@gmail.com>
2025-08-11 11:40:33 +0330

    show name, number,email in personal page

--- assets/personalpage.js
+++ assets/personalpage.js
@@ -1,5 +1,40 @@
 // سفارش هاس من
-
 const mybasket = document.querySelector(".basket");
 const panelLeft = document.querySelector(".panel-left")
+
+// نمایش نام کاربری ایمیل شماره تلفن
+
+const nameBox = document.querySelector(".name")
+const numberBox = document.querySelector(".number")
+const emailBox = document.querySelector(".email")
+
+async function loadUserPorfile(){
+    try{
+        const response = await fetch("http://localhost:8000/api/user",{
+            method:"GET",
+            headers : {
+                "Authorization":"Bearer" + localStorage.getItem("token"),
+                "Content-type":"application/json"
+            }
+        });
+
+        if(!response.ok){
+            throw new Error("خطا در دریافت اطلاعات کاربر" + response.status);
+        }
+        const data = await response.json();
+
+        nameBox.textContent = `${data.fname} ${data.lname}`;
+        numberBox.textContent = data.phone || "شماره ثبت نشده";
+        emailBox.textContent = data.email || "ایمیل ثبت نشده";
+    }catch(error){
+        console.error("خطا",error);
+    }
+}
+
+document.addEventListener("DOMContentLoaded",loadUserPorfile);
+
+
+
+
+
 
