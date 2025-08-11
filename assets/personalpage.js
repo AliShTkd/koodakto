10867848c3b0d782cdcff6995dd39b9e38825216
raambod <rambodheydari1@gmail.com>
2025-08-11 11:11:55 +0330

    personal page 3

--- assets/personalpage.js
+++ assets/personalpage.js
@@ -1,5 +1,38 @@
 // سفارش هاس من
-
-const mybasket = document.querySelector(".basket");
 const panelLeft = document.querySelector(".panel-left")
 
+async function openbasket() {
+    try{
+        const response = await fetch ("",{
+            method : "GET",
+            headers:{
+                "Authorization" : "bearer" + localStorage.getItem("token"),
+                "Accept":"application/json"
+            }
+        });
+        if(!response.ok){
+            throw new Error ("خطا در دریافت اطلاعات",response.status);
+        }
+        const data = await response.json();
+        let html = `<h2>سفارش  های من</h2><ul>`;
+        data.forEach(order => {
+            html +=`
+             <li>
+                    <strong>${order.product.name}</strong> 
+                    | تعداد: ${order.quantity} 
+                    | قیمت: ${order.product.price} تومان
+            </li>
+            `;
+        });
+        html +=`</ul>`
+
+        panelLeft.innerHTML= html;
+
+
+    }
+
+    catch(error){
+        console.error(error);
+        panelLeft.innerHTML = "<p>خطا در دریافت سفارش‌ها</p>";
+    }
+}
