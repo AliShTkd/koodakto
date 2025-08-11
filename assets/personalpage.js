f0f95a38aa33a1d124570f61992295021071e81d
raambod <rambodheydari1@gmail.com>
2025-08-11 11:43:58 +0330

    li show produvty comment personal page 5

--- assets/personalpage.js
+++ assets/personalpage.js
@@ -36,3 +36,45 @@ async function openbasket() {
         panelLeft.innerHTML = "<p>خطا در دریافت سفارش‌ها</p>";
     }
 }
+
+
+
+// کامنت ها و نظرات 
+
+
+async function opencomment() {
+    try{
+        const response = await fetch ("",{
+            method:"GET",
+            headers:{
+                "Authorization" : "bearer" + localStorage.getItem("token"),
+                "Accept":"application/json"
+            }
+        });
+        if(!response.ok){
+            throw new error ("خطا در دریافت اطلاعات", response.status);
+        }
+        const data = await response.json();
+        let html = `<h2>نظرات و کامنت های من</h2><ul>`;
+        data.forEach(Comment => {
+            html += 
+            `
+                <div class="comment-card">
+                    <img src="${comment.product_image}" alt="${comment.product_name}">
+                    <div class="comment-info">
+                        <h3>${comment.product_name}</h3>
+                        <p class="comment-text">${comment.text}</p>
+                        <span class="comment-date">${comment.date}</span>
+                    </div>
+                </div>
+            `;
+        });
+        html += `</div>`
+    }
+    catch(error){
+        console.error(error);
+        panelLeft.innerHTML = "<p>خطا در دریافت پیام ها</p>";
+    }
+}
+
+
